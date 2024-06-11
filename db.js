8f8977fdf97b8014e51a217fe15a449f52ea8e57
Pablo Bravo <pablobravodc@gmail.com>
2024-06-10 21:24:14 -0400

    conectando BD 7

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ const connection = mysql.createConnection({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  connectTimeout: 60000 // Aumentar el tiempo de espera a 60 segundos (valor en milisegundos)
 });
 
 connection.connect((err) => {
